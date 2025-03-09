1d5398d3a9cf28f0222d4103955dab46128c6734
tougshire <tougshire@tougshire.com>
2025-03-09 18:44:49 -0400

    Add allow_mfa_setup option

--- account/adapter.py
+++ account/adapter.py
@@ -6,3 +6,6 @@ class SDCAccountAdapter(DefaultAccountAdapter):
     def is_open_for_signup(self, request):
         default = super().is_open_for_signup(request)
         return getattr(settings, 'ACCOUNT_IS_OPEN_FOR_SIGNUP', default)
+
+    def allow_mfa_setup(self, request):
+        return getattr(settings, 'ACCOUNT_ALLOW_MFA_SETUP', False)
